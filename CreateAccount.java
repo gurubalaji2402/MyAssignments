2934efa993b133fbeecfa607ca7a87c03b1fea37
guru balaji <59954872+gurubalaji2402@users.noreply.github.com>
2025-12-14 10:44:28 +0530

    Update and rename week2.day1 to CreateAccount.java

---
+++ CreateAccount.java
@@ -0,0 +1,47 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LeaftapLogin {
+
+	public static void main(String[] args) {
+		
+		//Guest Mode starts Chrome with:
+//		No saved login sessions
+//		No cookies
+//		No cache
+//		No extensions
+//		No browser history
+//		
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--guest");
+		
+		//Initialize the; WebDriver (ChromeDriver).
+		ChromeDriver driver=new ChromeDriver(option);
+		
+		//Load the URL 
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		//Enter a username and password
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		
+		//create account
+		driver.findElement(By.linkText("Create Account")).click();
+		driver.findElement(By.className("inputBox")).sendKeys("guru");
+		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+		driver.findElement(By.name("numberEmployees")).sendKeys("10");
+		System.out.println(driver.getTitle());
+		driver.quit();		
+		
+		
+		
+}
+
+}
+
